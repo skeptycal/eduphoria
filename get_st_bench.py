6b59c9f9f385ca14b93fa004f1c6a546d5c618ed
skeptycal <skeptycal@gmail.com>
2019-03-31 14:40:26 -0500

    initial commit

--- get_st_bench.py
+++ get_st_bench.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
-###############################################################################
-# get_st_bench.py
-#
-# Returns: formatted student data from VISD Eduphoria
-#
-# Account and login required
-# https://victoria.schoolobjects.com/eduphoria_webcontrols/Login.aspx?ReturnUrl=%2feduphoria_webcontrols%2fApplications.aspx
-###############################################################################
+""" module get_st_bench
 
-import requests
-import markdown  # print markdown.markdown('**Excellent**')"
+    collect and return formatted student data from Eduphoria
+
+    Account and login required
+    https://victoria.schoolobjects.com/eduphoria_webcontrols/Login.aspx?ReturnUrl=%2feduphoria_webcontrols%2fApplications.aspx
+    """
+
+from typing import AnyStr  # , List, Dict
+import requests as r
+import markdown as m
+
+eduphoria_page: AnyStr = 'https://victoria.schoolobjects.com/eduphoria_webcontrols/Login.aspx?ReturnUrl=%2feduphoria_webcontrols%2fApplications.aspx'
+
+# ! html test
+url: AnyStr = 'https://www.google.com'
+response = r.get(url)
+print(m.markdown(response.apparent_encoding))
+print(m.markdown(response.text))
+
+# ! json test
+url = 'https://jsonplaceholder.typicode.com/todos/1'
+response = r.get(url)  # execute get request
+print("Get: ", url)
+print("Status code: ", response.status_code)  # print http response code
+print("Text: ", response.text)  # print formatted JSON response
+print("Content: ", response.content)  # print 'content'
+
+url = eduphoria_page
+
+# response = reqs.get(eduphoria_page)
